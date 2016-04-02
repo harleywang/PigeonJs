fcc6d924964ef1a834db1dd4a867aad741f43a34
HarleyWang <haley.wang.vip@gmail.com>
2016-04-02 21:18:00 +0800

    删除clearHandlers方法外部调用，添加React中使用的example。新增支持单一事件和群组的触发和移除

--- README.md
+++ README.md
@@ -3,42 +3,97 @@ PigeonJs
 
 `PigeonJs` 是一个轻量级自定义事件或观察者模式的实现。
 
-####特点：
+#### 特点：
 
 * 小巧，不依赖任何`lib`
 * 单例模式实现，任意地方可添加订阅，移除订阅，清除事件类型；
+* 支持单事件触发和群组触发，单事件移除和群组移除；
 * 移除订阅只需通过`namespace`命令空间，因为有时添加订阅和移除阅订不在一个作用域下, 这样会带来极大的方便；
 * 安全的`handlers`事件存储，不会被无意中覆盖；
 
 
+#### NameSpace格式：
 
-####NameSpace格式：
-
-
-`type.key` : type为事件的类型，key为订阅的唯一标识符
-
+`type.key` : type为事件的类型，表示群组；key为订阅的唯一标识符
+> 注意：多层命名空间（例如：uxdc.haleywang.age），第二层后面的部分会被忽略掉
 
 
-####Example：
+#### Example：
 
 ```javascript
+
 // 添加订阅
 PigeonJs.addHandler('uxdc.haleywang', function(ns, event){
-    console.log(ns, event.message, 'My key is haleywang');
+    console.log(ns, event.message, '我是haleywang，收到!');
+});
+
+// 添加订阅
+PigeonJs.addHandler('uxdc.ice', function(ns, event){
+    console.log(ns, event.message, '我是ice，收到!');
 });
 
 // 添加订阅
 PigeonJs.addHandler('uxdc.ouyi', function(ns, event){
-    console.log(ns, event.message, 'My key is ouyi');
+    console.log(ns, event.message, '我是ouyi，收到!');
 });
 
+
 // 触发
-PigeonJs.trigger('uxdc', {message:"Hello PigeonJs!"});
+PigeonJs.trigger('uxdc.ouyi', {message:"2:00 来duang2会议室开会!"});
+PigeonJs.trigger('uxdc.haleywang', {message:"4:00 上线!"});
+PigeonJs.trigger('uxdc.ice', {message:"带个早点上楼!"});
+
+// 触发群组
+PigeonJs.trigger('uxdc', {message:"@all：明天放假一天!"});
 
 // 移除订阅
-PigeonJs.removeHandler('uxdc.wanghailiang');
+PigeonJs.removeHandler('uxdc.haleywang');
+// 触发群组
+PigeonJs.trigger('uxdc', {message:"@all：发周报!"});
 
-// 清除整个事件类型
-PigeonJs.clearHandlers('uxdc');
+// 移除群组
+PigeonJs.removeHandler('uxdc');
 
+```
+
+#### 在React中使用：
+使用 `PigeonJs` 在组件之间进行通讯或数据共享：
+
+```javascript
+// 快捷菜单
+var ShortcutMenu = React.createClass({
+    getInitialState: function() {
+        return {selected: this.props.selected};
+    },
+    show: function(){
+        this.refs.shortcut_menu.style.display = 'block';
+    },
+    hide: function(){
+        this.refs.shortcut_menu.style.display = 'none';
+    },
+    componentDidMount: function(){
+        var that = this;
+        PigeonJs.addHandler('ShortcutMenu.onShow', function(ns, event){
+            that.show();
+        });
+    },
+    componentWillUnmount: function(){
+        PigeonJs.removeHandler('ShortcutMenu.onShow');
+    },
+    render: function() {
+        return (
+            <div ref="shortcut_menu" className="shortcut_menu"></div>
+        );
+    }
+});
+ReactDOM.render(<ShortcutMenu />, document.getElementById('shortcut_menu'));
+```
+
+在其它组件中触发：
+
+```javascript
+// 更多快捷菜单
+onMore: function(e){
+    PigeonJs.trigger('ShortcutMenu');
+}
 ```
--- src/pigeon.js
+++ src/pigeon.js
@@ -1,7 +1,7 @@
 /**
  * pigeon.js
  *
- * Release 1.0.0
+ * Release 1.2.0
  * @author <a href="mailto:haley.wang.vip@gmail.com">Harley Wang</a>
  * @describe  一个轻量级自定义事件或观察者模式的实现。
  */
@@ -20,12 +20,11 @@
     var handlers = {};
     var splitStr = function(ns){
         var values = ns.split('.', 2);
-        if(values.length !== 2){
-            throw "namespace format error!";
-        }
         return {'type':values[0], 'key':values[1]};
+    },
+    clearHandlers = function(type){
+        delete handlers[type];
     };
-
     /**
      * 添加事件
      * @param  {String} namespace   命名空间，格式必须为：type.key
@@ -42,11 +41,14 @@
     };
     /**
      * 移除事件
-     * @param  {string} namespace    命名空间，格式必须为：type.key
+     * @param  {string} namespace    命名空间，为：type.key 或 type
      */
     PigeonJs.removeHandler = function(namespace){
         var ns = splitStr(namespace);
-        if(handlers[ns.type] instanceof Array){
+        if(ns.type && !ns.key){
+            clearHandlers(ns.type);
+        }
+        else if(handlers[ns.type] instanceof Array){
             var events = handlers[ns.type];
             for(var i=0,len=events.length; i<len; i++){
                 if(events[i]['unique'] == ns.key){
@@ -56,31 +58,31 @@
             }
 
             if(events.length == 0){
-                this.clearHandlers(ns.type);
+                clearHandlers(ns.type);
             }
         }
     };
     /**
      * 触发器
-     * @param  {string} type                                        事件类型，为命令空间的前面部分
-     * @param  {Object|Array|String|Boolean|Number|Function} data   要传递的数据
+     * @param  {string} namespace                                   命名空间，为：type.key 或 type
+     * @param  {Object|Array|String|Boolean|Number|Function} data   要传递的数据，可选
      */
-    PigeonJs.trigger = function(type, data){
-        if(handlers[type] instanceof Array){
-            var events = handlers[type];
+    PigeonJs.trigger = function(namespace, data){
+        var ns = splitStr(namespace);
+
+        if(handlers[ ns.type ] instanceof Array){
+            var events = handlers[ns.type];
             for(var i=0,len=events.length; i<len; i++){
-                events[i]['handler'](type, data);
+                if(ns.key){
+                    if(events[i]['unique'] == ns.key){
+                        events[i]['handler'](ns, data);
+                        break;
+                    }
+                }else{
+                    events[i]['handler'](ns, data);
+                }
             }
-        }else{
-            throw "event type error!";
         }
     };
-    /**
-     * 清除事件
-     * @param  {string} type    事件类型，为命令空间的前面部分
-     */
-    PigeonJs.clearHandlers = function(type){
-        delete handlers[type];
-    }
 
 }));
